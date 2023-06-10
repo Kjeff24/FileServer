362ad74b2df590b535921f83b0cfa53d3c296835
kjeff24 <jeffreyarthur123@gmail.com>
2023-06-10 19:18:05 +0000

    send file to others, email and download count for a specific user

--- myapp/views/homepage.py
+++ myapp/views/homepage.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import FileResponse
-from myapp.models import File
+from myapp.models import File, UserActivities
 from urllib.parse import urlencode
-from urllib.parse import quote
 from django.core.mail import EmailMessage
 from django.conf import settings
-import threading
 from django.template.loader import render_to_string
 from django.contrib import messages
 import mimetypes
@@ -19,10 +16,10 @@ from django.http import StreamingHttpResponse
 
 
 def home(request):
-    
+
     # retrieves a query parameter value from an HTTP GET request
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    
+
     # Q filters object that can be encapsulated logically
 
     pdf_files = File.objects.filter(
@@ -32,7 +29,7 @@ def home(request):
         Q(file_type__icontains=q),
         file_type='pdf'
     ).order_by('-created')
-    
+
     image_files = File.objects.filter(
         Q(title__icontains=q) |
         Q(description__icontains=q) |
@@ -40,7 +37,7 @@ def home(request):
         Q(file_type__icontains=q),
         file_type='image'
     ).order_by('-created')
-    
+
     audio_files = File.objects.filter(
         Q(title__icontains=q) |
         Q(description__icontains=q) |
@@ -48,7 +45,7 @@ def home(request):
         Q(file_type__icontains=q),
         file_type='audio'
     ).order_by('-created')
-    
+
     video_files = File.objects.filter(
         Q(title__icontains=q) |
         Q(description__icontains=q) |
@@ -69,22 +66,17 @@ def home(request):
 @login_required(login_url='login')
 def downloadFile(request, pk):
     file = File.objects.get(id=pk)
-    
+
+    # Get or create UserActivities object for the logged-in user
+    user_activities, _ = UserActivities.objects.get_or_create(user=request.user)
+
+    # Increase the download count for the user
+    user_activities.download_count()
+
     # increase number of downloads of a particular
     file.downloads_count()
     messages.success(request, 'Download successfully.')
     return redirect('home')
-
-
-# allows the email to be sent asynchronously while the main program continues to execute.
-class EmailThread(threading.Thread):
-
-    def __init__(self, email):
-        self.email = email
-        threading.Thread.__init__(self)
-
-    def run(self):
-        self.email.send()
 
 
 @login_required(login_url='login')
@@ -93,30 +85,48 @@ def emailFile(request, pk):
     # Retrieve the file object
     file_obj = File.objects.get(id=pk)
 
-    # Create an EmailMessage object
-    email = EmailMessage(
-        subject='File Attachment',
-        body=render_to_string('myapp/send_email.html',
-                              {'user': user, 'file_obj': file_obj}),
-        from_email=settings.EMAIL_FROM_USER,
-        to=[user.email]
-    )
+    # Get or create UserActivities object for the logged-in user
+    user_activities, _ = UserActivities.objects.get_or_create(user=user)
+
+    if request.method == 'POST':
+        # Get the email from the form
+        user_email = request.POST.get('email')
+        # Create an EmailMessage object
+        email = EmailMessage(
+            subject='File Attachment',
+            body=render_to_string('myapp/send_email.html',
+                                {'user': user_email, 'from_user': user, 'file_obj': file_obj}),
+            from_email=settings.EMAIL_FROM_USER,
+            to=[user_email]
+        )
+        print(f" email {user_email} ")
+    else:
+        # Create an EmailMessage object
+        email = EmailMessage(
+            subject='File Attachment',
+            body=render_to_string('myapp/send_email.html',
+                                {'user': user, 'file_obj': file_obj}),
+            from_email=settings.EMAIL_FROM_USER,
+            to=[user.email]
+        )
 
     # Open the file in binary mode and read only
     with open(file_obj.file.path, 'rb') as f:
         file_content = f.read()
-        
+
         # Guess the content type of the file based on it's extension
         # Disregard the encoding part using the underscore
         content_type, _ = mimetypes.guess_type(file_obj.file.path)
         print(f'{content_type} -- content tupes')
-        
+
         # Attach the file to the email
         email.attach(file_obj.file.name, file_content, content_type)
 
-    # Send the email using a different process other than the main program execution
-    if not settings.TESTING:
-        EmailThread(email).start()
+    # send email
+    email.send()
+
+    # Increase the download count for the user
+    user_activities.email_count()
 
     # Update the file's emails_sent count
     file_obj.emails_sent_count()
@@ -134,7 +144,7 @@ def previewPdf(request, pk):
         with open(file.file.path, 'rb') as f:
             for chunk in iter(lambda: f.read(8192), b''):
                 yield chunk
-                
+
     # use StreamingHttpResponse to load pdf content
     response = StreamingHttpResponse(
         file_iterator(), content_type='application/pdf')
